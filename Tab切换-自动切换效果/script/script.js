37139a52fcbb636dbb56547f6352cd1102a53efa
liqilong <634505724@qq.com>
2017-11-17 12:19:53 +0800

    自动切换效果

--- Tab切换-自动切换效果/script/script.js
+++ Tab切换-自动切换效果/script/script.js
@@ -12,19 +12,44 @@ function tab() {
     //获取所有的页签和要切换的内容
     var lis = $('notice-tit').getElementsByTagName('li');
     var divs = $('notice-con').getElementsByTagName('div');
+
+    //遍历每一个页签且给他们绑定事件
+    for(var i=0;i<lis.length;i++) {
+        lis[i].id = i;
+        lis[i].onmouseover = function() {
+            clearInterval(timer);
+            changeOption(this.id);               
+        }
+
+        lis[i].onmouseout = function() {
+            timer = setInterval(autoPlay, 2000);
+        }
+    }
+
+    if(timer){
+        clearInterval(timer);
+        timer = null;
+    }
     //添加定时器，改变当前高亮的索引
-    timer = setInterval(function() {
+    timer = setInterval(autoPlay, 2000);
+
+    function autoPlay() {
         index++;
+
         if(index>=lis.length) {
             index = 0;
         }
+        changeOption(index); //index = 1;
+    }
 
+    function changeOption(curIndex) {
         for(var j =0;j<lis.length;j++) {
             lis[j].className = '';
             divs[j].style.display = 'none';
         }
         //高亮显示当前页签
-        lis[index].className = 'select';
-        divs[index].style.display = 'block';
-    }, 2000);
+        lis[curIndex].className = 'select';
+        divs[curIndex].style.display = 'block';
+        index = curIndex;
+    }
 }
